b4ef12c5420db5b8347a78a030d937c64db7c246
Nauel Gamero <g.nauel@gmail.com>
2018-03-28 12:32:48 +0200

    Fin de la sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { APP_ROUTES } from './app-routes';
 // MODULOS
 import { PagesModule } from './pages/pages.module';
 
+// SERVICIOS
+import { ServiceModule } from './services/service.module';
+
+
 // COMPONENTES
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './login/register.component';
@@ -24,7 +28,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
